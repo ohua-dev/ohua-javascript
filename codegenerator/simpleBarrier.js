6c7148e16d76fdb9aaf398429a45ad2401e9c4d7
TheL1ne <benrie7@gmail.com>
2018-06-05 10:44:33 +0200

    added simple Barrier

---
+++ codegenerator/simpleBarrier.js
@@ -0,0 +1,50 @@
+// found here: https://github.com/jimhigson/simple-barrier/blob/master/src/simple-barrier.js
+module.exports = function simpleBarrier() {
+
+   var requiredCallbacks = 0,
+       doneCallbacks = 0,
+       startTime = Date.now(),
+       results = [];
+
+   function defaultCallback(err, data) {
+      return data;
+   }
+
+   var instance = {
+
+      waitOn:function(callback){
+
+         var callbackIndex = requiredCallbacks;
+         callback = callback || defaultCallback;
+
+         requiredCallbacks++;
+
+         return function(){
+            var result = callback.apply(this,arguments);
+
+            results[callbackIndex] = result;
+
+            doneCallbacks++;
+
+            if( requiredCallbacks === doneCallbacks ) {
+               instance.duration = Date.now() - startTime;
+               instance.endWithCallback(results);
+            }
+         }
+      },
+
+      endWith: function(fn) {
+         instance.endWithCallback = fn;
+      },
+
+      getRequiredCallbacks: function() {
+      	 return requiredCallbacks;
+      },
+
+      getDoneCallbacks: function() {
+      	 return doneCallbacks;
+      }
+   };
+
+   return instance;
+};
